0d1c3e93a355a62784a2f76ecccd2032e05b5b96
deltaermes <ermes.ingeniero@gmail.com>
2022-08-19 11:43:18 -0500

    terminado tablas de usuarios

--- render/app/Views/Contenido/contenidoUsuario.php
+++ render/app/Views/Contenido/contenidoUsuario.php
@@ -27,7 +27,7 @@ Usuarios
                 <div class="container">
                     <div class="card-body px-0 pt-0 pb-2">
                         <div class="table-responsive p-0">
-                            <table id="usuarios" class="table align-items-center mb-0">
+                            <table id="usuarios" class="align-items-center mb-0">
                                 <thead>
                                     <tr>
                                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nombre</th>
