4b133b51ffdb52fa33b1c7ce67d79b1f14f96c52
ju4nSh <110780968+ju4nSh@users.noreply.github.com>
2022-09-01 14:29:09 -0500

    ModificarMercadoShop

--- render/app/Controllers/MercadoShop.php
+++ render/app/Controllers/MercadoShop.php
@@ -7,31 +7,56 @@ use App\Models\Usuarios;
 
 class MercadoShop extends Controller
 {
-    public function getValidarMercadoShop(){
+    public function getValidarMercadoShop()
+    {
+        $token = "APP_USR-4332857485021545-090110-7dce48f88fef304dbcfb6e48a18226e9-833930674";
         $ssesion = \Config\Services::session();
         $db = \Config\Database::connect();
         $builder = $db->table('users');
         $id = $ssesion->get("user");
         $rol = $ssesion->get("rol");
-        if($rol == 0){
+        if ($rol == 0) {
             $data_array = array('Usuario' => $id);
-        $datos = $builder->select('userId')->where($data_array)->get()->getResultArray();
-        foreach ($datos as $variable) {
-            $uri = 'https://api.mercadolibre.com/sites/MCO/search?seller_id='.$variable['userId'];
-            $conexion = curl_init();
-            $token = "APP_USR-4332857485021545-083014-e5fa0cb97af6419f78a945df0e0bc0c6-833930674";
-            curl_setopt($conexion, CURLOPT_URL, $uri);
-            curl_setopt($conexion, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($conexion, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json', "Authorization: Bearer $token"));
-            // curl_setopt($conexion, CURLOPT_POSTFIELDS, json_encode($datos));
-            curl_setopt($conexion, CURLOPT_RETURNTRANSFER, 1);
-    
-            $r = curl_exec($conexion);
-            $m = json_decode($r,true);
-            echo json_encode($m);
+            $datos = $builder->select('userId')->where($data_array)->get()->getResultArray();
+            foreach ($datos as $variable) {
+                $this->getConsultaMercadoShop($variable['userId'],$token);
+            }
+        } else {
+            $data_array = array('Usuario' => $id);
+            $datos1 = $builder->select('Creator')->where($data_array)->get()->getResultArray();
+            foreach ($datos1 as $variable1) {
+                $data_array1 = array('id' => $variable1['Creator'][0]);
+                $datos3 = $builder->select('userId')->where($data_array1)->get()->getResultArray();
+                foreach ($datos3 as $variable2) {
+                    $this->getConsultaMercadoShop($variable2['userId'],$token);
+                }
+            }
         }
+    }
+
+    public function getConsultaMercadoShop($user,$token)
+    {
+        $uri = 'https://api.mercadolibre.com/sites/MCO/search?seller_id=' .$user ;
+        $conexion = curl_init();
+        
+        curl_setopt($conexion, CURLOPT_URL, $uri);
+        curl_setopt($conexion, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($conexion, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json', "Authorization: Bearer $token"));
+        // curl_setopt($conexion, CURLOPT_POSTFIELDS, json_encode($datos));
+        curl_setopt($conexion, CURLOPT_RETURNTRANSFER, 1);
+
+        $r = curl_exec($conexion);
+        $m = json_decode($r, true);
+        $respuesta = false;
+        foreach ($m["seller"]["tags"] as $busca) {
+            if ($busca === "mshops") {
+                $respuesta = true;
+            }
         }
-        
-        
+        if ($respuesta == true) {
+            echo "Bien";
+        } else {
+            echo "No puede ejecutar esta funcion";
+        }
     }
 }
