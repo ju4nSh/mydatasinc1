b1636471f1d5971220c48814e376549eac578bc5
ju4nSh <110780968+ju4nSh@users.noreply.github.com>
2022-09-05 16:29:26 -0500

    Probando comandos Cli

--- render/app/Config/Routes.php
+++ render/app/Config/Routes.php
@@ -149,4 +149,6 @@ $routes->post('modificarRolNull', 'Rol::modificarRolNull');
 
 $routes->post('publicarMasivo', 'Productos::publicarMasivo');
 
+$routes->cli('Prueba/(:segment)',  'Personas::Prueba/$1');
+$routes->cli('PausarLote', 'Personas::PausarLote');
 
--- render/app/Controllers/Personas.php
+++ render/app/Controllers/Personas.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use CodeIgniter\Controller;
 use App\Models\Usuarios;
+use CodeIgniter\CLI\CLI;
 
 class Personas extends Controller
 {
@@ -88,6 +89,98 @@ class Personas extends Controller
         }
         
         echo json_encode($array);
+    }
+    public function Prueba($to){
+        $db = \Config\Database::connect();
+        $builder = $db->table('productos');
+        $countEstado1=0;
+        $countCantidadMen15=0;
+        $countCantidadMen50=0;
+        $countCantidadMay50=0;
+        $countEstado0=0;
+        $datos = $builder->select('nombre,cantidad,precio,estado')->get()->getResultArray();
+        $thead = [
+            CLI::color('Nombre', 'white'),
+            CLI::color('Cantidad', 'white'), 
+            CLI::color('Precio', 'white'),
+            CLI::color('Estado', 'white'),  
+        ];
+        CLI::newLine();
+        CLI::print("Tabla Productos");
+
+        foreach($datos as $variable){
+            if($variable["estado"]==1){
+                $countEstado1+=1; 
+                if($variable["cantidad"]>=15 &&  $variable["cantidad"]<50){
+                    $countCantidadMen50+=1;
+                    $tbody []= [
+                        CLI::color($variable["nombre"],'white'),
+                        CLI::color($variable["cantidad"],'yellow'),
+                        CLI::color($variable["precio"],'white'),
+                        CLI::color($variable["estado"],'green')
+                    ];
+                }else if($variable["cantidad"]>=50){
+                    $countCantidadMay50+=1;
+                    $tbody []= [
+                        CLI::color($variable["nombre"],'white'),
+                        CLI::color($variable["cantidad"],'green'),
+                        CLI::color($variable["precio"],'white'),
+                        CLI::color($variable["estado"],'green')
+                    ];
+                }else{
+                    $countCantidadMen15+=1;
+                    $tbody []= [
+                        CLI::color($variable["nombre"],'white'),
+                        CLI::color($variable["cantidad"],'red'),
+                        CLI::color($variable["precio"],'white'),
+                        CLI::color($variable["estado"],'green')
+                    ];
+                }
+            }else{
+                $countEstado0+=1;
+                $tbody []= [
+                    CLI::color($variable["nombre"],'light_red'),
+                    CLI::color($variable["cantidad"],'light_red'),
+                    CLI::color($variable["precio"],'light_red'),
+                    CLI::color($variable["estado"],'light_red')
+                ];
+            }
+        };
+        CLI::table($tbody, $thead);
+        CLI::newLine();
+        CLI::print(count($datos). ' Total de productos' );CLI::newLine();
+        CLI::print("Resumen tabla : ".$countEstado1. ' Productos Activos.' );CLI::newLine();
+        CLI::print($countEstado0. ' Productos Inactivos.' );CLI::newLine();
+        CLI::print('Detalle Tabla' );CLI::newLine();
+        CLI::print($countCantidadMen15. ' Productos con una cantidad menor de 15.' );CLI::newLine();
+        CLI::print($countCantidadMen50. ' Productos con una cantidad menor de 50.' );CLI::newLine();
+        CLI::print($countCantidadMay50. ' Productos con una cantidad mayor de 50.' );CLI::newLine();
+    }
+
+    public function PausarLote($to){
+        $db = \Config\Database::connect();
+        $builder = $db->table('productos');
+        $datos = $builder->select('codigo')->like("nombre",$to,'before')->limit(2)->get()->getResultArray();
+        foreach($datos as $variable){
+            CLI::newLine();
+            $uri ="https://api.mercadolibre.com/items/".$variable["codigo"];
+
+		$descripcion = [
+			"status"=>"paused"
+		];
+		$conexion = curl_init();
+		$token ="APP_USR-4332857485021545-090516-f69a7b649907e5a96228a68c6007cf56-833930674";
+		curl_setopt($conexion, CURLOPT_URL, $uri);
+		curl_setopt($conexion, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($conexion, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json', "Authorization: Bearer $token"));
+		curl_setopt($conexion, CURLOPT_POSTFIELDS, json_encode($descripcion));
+		curl_setopt($conexion, CURLOPT_RETURNTRANSFER, 1);
+
+		$r = curl_exec($conexion);
+		curl_close($conexion);
+		print_r($r);
+        CLI::newLine();
+        }
     }
     
     
